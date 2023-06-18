dddb059ca419bdd730d9afc28da69eb461a00321
Pakizamutallim <127186291+Pakizamutallim@users.noreply.github.com>
2023-06-18 14:28:55 +0400

    Add files via upload

---
+++ admin panel/assets/js/main.js
@@ -0,0 +1,16 @@
+const addBtn = document.querySelector(".add-btn")
+addBtn.addEventListener("click", ()=>{
+	window.location.href = ""
+})
+
+document.querySelectorAll(".edit") 
+edit.forEach(btn => {
+	btn.addEventListener("click", )
+});
+
+
+
+
+document.addEventListener("click", ()=>{
+	const target = e.target.closest(".edit");
+})
